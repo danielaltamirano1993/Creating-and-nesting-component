2754155a8bc83a2d6a7c4a1d5135ba03cdfe860d
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2023-08-15 10:19:24 -0500

    c5

--- app.js
+++ app.js
@@ -19,3 +19,42 @@ function MyButton() {
 
   return <button onClick={handleClick}>Clicked {count} times</button>;
 }
+
+//Using Hooks 
+
+export default function MyApp() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
+    </div>
+  );
+}
+
+function MyButton() {
+  // ... we're moving code from here ...
+}
+
+//
+export default function MyApp() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+    </div>
+  );
+}
