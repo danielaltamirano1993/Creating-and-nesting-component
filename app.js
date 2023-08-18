e8a93e8fb078a1baf852ec699f357f2e9cdd10cd
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2023-08-17 19:46:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function MyButton() {
   const [count, setCount] = useState(0);
 
   function handleClick() {
-    setCount(count + 1);
+    setCount(count + 1);u
   }
 
   return <button onClick={handleClick}>Clicked {count} times</button>;
